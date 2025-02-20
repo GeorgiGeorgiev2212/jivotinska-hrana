388d5e9aa624475772baf304cdee1815e2f65c22
Georgi Georgiev <Georgi Georgiev@DESKTOP-9FI4TKH>
2025-02-20 15:50:23 +0200

    Add project files.

---
+++ jivotinska hrana/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Napishete kolko pari imate");
+double money = double.Parse(Console.ReadLine());
+Console.WriteLine("Napishete kolko kutii kucheshka hrana ste kupili");
+int dogfood = int.Parse(Console.ReadLine());
+Console.WriteLine("Napishete kolko kutii koteshka hrana ste kupili");
+int catfood = int.Parse(Console.ReadLine());
+
+
+double dogfoodprice = 2.50;
+double catfoodprice = 4.00;
+
+if (money > catfoodprice)
+{
+    if (money > dogfoodprice)
+    {
+
+        Console.WriteLine("Parite sa dostatuchni za kucheshka hrana");
+
+    }
+    Console.WriteLine("Parite sa dostatuchni za koteshka hrana");
+
+}
+else
+{
+
+    Console.WriteLine("Parite ne sa dostatuchni");
+}
+
+
+
+Console.WriteLine("Kucheshkata hrana struva: ");
+Console.WriteLine(dogfood * dogfoodprice); 
+
+Console.WriteLine("Koteshkata hrana struva: ");
+Console.WriteLine(catfood * catfoodprice);
